a37d5c86e68c7110593cd4a49c6532dff7a29e82
LEMMYYYYY <remi.jubinville@hotmail.ca>
2026-03-20 17:18:09 -0400

    Implemented Project class

--- personal_task_manager_system/src/main/java/java/com/taskmanager/models/Project.java
+++ personal_task_manager_system/src/main/java/java/com/taskmanager/models/Project.java
@@ -1,4 +1,51 @@
 package java.com.taskmanager.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Project {
+    private String id;
+    private String name;
+    private String description;
+
+    private List<Collaborator> collaborators;
+
+    public Project(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.collaborators = new ArrayList<>();
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Collaborator> getCollaborators() {
+        return collaborators;
+    }
+
+    public void addCollaborator(Collaborator collaborator) {
+        this.collaborators.add(collaborator);
+    }
 }
